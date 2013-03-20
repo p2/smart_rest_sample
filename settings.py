6b647e15670332163addf0d94d38213e8db42ed5
Arjun Sanyal <arjun.sanyal@childrens.harvard.edu>
2013-03-20 12:59:51 -0400

    Updates for endpoints change

--- settings.py
+++ settings.py
@@ -1,13 +1,15 @@
 # configure servers
 
-ENDPOINTS = {
-    "http://sandbox-rest.smartplatforms.org:7000": {
+ENDPOINTS = [
+    {
+        "url": "http://sandbox-rest.smartplatforms.org:7000",
         "name": "REST Sandbox",
         "app_id": "rest-example@apps.smartplatforms.org",
         "consumer_key": "rest-example@apps.smartplatforms.org",
         "consumer_secret": "hAkIjrDeBpJlfeJl"
     },
-    "http://localhost:7000": {
+    {
+        "url": "http://localhost:7000",
         "name": "Localhost",
         "app_id": "rest-example@apps.smartplatforms.org",
         "consumer_key": "rest-example@apps.smartplatforms.org",
